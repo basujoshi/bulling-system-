773d548b2671edb7e97c55f2bd9524877a7fe611
basujoshi <joshibasu12345@gmail.com>
2026-09-18 11:44:01 +0545

    first commit

---
+++ js/firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase configuration for Billing Multi-User
+const firebaseConfig = {
+  apiKey: "AIzaSyCrkGEIgNlAsmOtWtrlCkelYrqDd7j-SoA",
+  authDomain: "public-project-4d672.firebaseapp.com",
+  databaseURL: "https://public-project-4d672-default-rtdb.firebaseio.com",
+  projectId: "public-project-4d672",
+  storageBucket: "public-project-4d672.firebasestorage.app",
+  messagingSenderId: "265276656656",
+  appId: "1:265276656656:web:40d1c9098c181c97b88e95",
+  measurementId: "G-PY7LB9JY7F"
+};
